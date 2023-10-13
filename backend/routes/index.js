fd21d9a9f01f85618ebd4f83378001bec896be41
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-10-13 21:36:40 +0200

    Setup test route in API

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const message = {
+  author: 'TestAuthor',
+  text: 'TestText',
+  timestamp: 'today'
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/test', function(req, res, next) {
+  res.json(message)
+})
 
 module.exports = router;
