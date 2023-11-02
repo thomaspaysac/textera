5ffa6f4baef327632434b8e347453099c8438786
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-11-02 11:34:50 +0100

    Update ChangeAvatar.jsx

--- client/src/pages/ChangeAvatar.jsx
+++ client/src/pages/ChangeAvatar.jsx
@@ -31,6 +31,7 @@ export const ChangeAvatarPage = () => {
     if (window.confirm('Do you really want to delete your avatar?')) {
       const data = { userID: localStorage.user_id };
       await fetch('http://localhost:3000/user/edit/avatar/delete', {
+      //await fetch('https://textera-production.up.railway.app/user/edit/avatar/delete', {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
