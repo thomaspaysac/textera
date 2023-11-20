bec8578366937365e7ca257bda8c4ea66c202fb9
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-11-20 21:22:48 +0100

    Add compression and helmet to middleware chain

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,8 @@ const LocalStrategy = require("passport-local").Strategy;
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const compression = require("compression");
+const helmet = require("helmet");
 
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/user');
@@ -80,6 +82,8 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
+app.use(compression()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
